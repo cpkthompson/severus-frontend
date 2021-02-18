db1c668af721c8f7e6b8229639b45707bd6391ee
Charles Thompson <cpkthompson@gmail.com>
2021-02-18 15:35:27 +0000

    allows selection of images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,18 @@ export class AppComponent {
     'MEST Incubator (Ghana)',
     'MEST Incubator (Kenya)',
   ];
+  signatureImages = [
+    'assets/images/signatures/Cap2.1.png',
+    'assets/images/signatures/Cap2.2.png',
+    'assets/images/signatures/Cap2.3.png',
+    'assets/images/signatures/Cap2.4.png',
+    'assets/images/signatures/Cap2.5.png',
+    'assets/images/signatures/Cap2.6.png',
+    'assets/images/signatures/Email%20Signatures.png',
+    'assets/images/signatures/Email%20Signatures%20(1).png',
+    'assets/images/signatures/Email%20Signatures%20(2).png',
+  ];
+
   profileForm = new FormGroup({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
@@ -21,10 +33,11 @@ export class AppComponent {
     title: new FormControl(''),
     team: new FormControl(),
     skypeId: new FormControl(''),
-    country1: new FormControl('GH'),
+    country1: new FormControl(''),
     phoneNumber1: new FormControl(''),
-    country2: new FormControl('GH'),
+    country2: new FormControl(''),
     phoneNumber2: new FormControl(''),
   });
   secondForm = false;
+  signatureImage = this.signatureImages[1];
 }
