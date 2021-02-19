4ffd671cd6ef25ecb1b3f9c004357adaa6d0a3e3
Charles Thompson <cpkthompson@gmail.com>
2021-02-19 17:14:33 +0000

    minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   teams = [
